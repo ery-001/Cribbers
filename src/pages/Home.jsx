cc036c742134e63c6c5f78fe7204a6a093475421
ery-001 <ajayierioluwa123@gmail.com>
2024-08-07 15:25:18 +0100

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from '../components/Hero'
+import Mission from '../components/Mission'
+import Reviews from '../components/Reviews'
+import Reputable from '../components/Reputable'
+import ReachUs from '../components/ReachUs'
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <Mission />
+        <Reviews />
+        <Reputable />
+        <ReachUs />
+  </>
+  )
+}
+
+export default Home
